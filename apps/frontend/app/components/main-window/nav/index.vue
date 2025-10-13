6cbce3aad913e051eb2e455aa601b1d285bd2bc4
Junhsiun <xjuunn@gmail.com>
2025-10-13 16:59:01 +0800

    feat:允许用户最大化、最小化、窗口化、关闭窗口
    * 将字体移动到public中

--- apps/frontend/app/components/main-window/nav/index.vue
+++ apps/frontend/app/components/main-window/nav/index.vue
@@ -1,5 +1,58 @@
 <template>
-    <div class="border-b border-base-content/10 navbar min-h-[3.5rem] px-3" data-tauri-drag-region>
-        <span class="font-modernia">Junc</span>
+    <div class="border-b border-base-content/10 navbar min-h-[3.5rem] px-3 select-none" data-tauri-drag-region>
+        <div class="navbar-start" data-tauri-drag-region>
+            <span class="font-modernia text-base-content" data-tauri-drag-region>Junc</span>
+        </div>
+        <div class="navbar-end" data-tauri-drag-region>
+            <span v-show="isTauri()" class="flex gap-3">
+                <icon @click="minimizeWindow" class="text-base-content/60 hover:text-base-content cursor-pointer"
+                    name="mingcute:minimize-line" size="0.9rem"></icon>
+                <icon v-if="!maximized" @click="maximizeWindow"
+                    class="text-base-content/60 hover:text-base-content cursor-pointer" name="mingcute:square-line"
+                    size="0.9rem"></icon>
+                <icon v-else @click="unmaximizeWindow"
+                    class="text-base-content/60 hover:text-base-content cursor-pointer" name="mingcute:copy-2-line"
+                    size="0.9rem"></icon>
+                <icon @click="closeWindow" class="text-base-content/60 hover:text-error cursor-pointer"
+                    name="mingcute:close-line" size="0.9rem"></icon>
+            </span>
+        </div>
     </div>
 </template>
+
+<script setup lang="ts">
+import { getCurrentWindow } from '@tauri-apps/api/window'
+
+const maximized = ref(false)
+
+async function checkMaximized() {
+    maximized.value = await getCurrentWindow().isMaximized()
+}
+
+async function minimizeWindow() {
+    await getCurrentWindow().minimize()
+}
+
+async function maximizeWindow() {
+    await getCurrentWindow().maximize()
+    maximized.value = true
+}
+
+async function unmaximizeWindow() {
+    await getCurrentWindow().unmaximize()
+    maximized.value = false
+}
+
+async function closeWindow() {
+    await getCurrentWindow().close()
+}
+
+onMounted(async () => {
+    if (isTauri()) {
+        await checkMaximized()
+        getCurrentWindow().onResized(async () => {
+            maximized.value = await getCurrentWindow().isMaximized()
+        })
+    }
+})
+</script>
