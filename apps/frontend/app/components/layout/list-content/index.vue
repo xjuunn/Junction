2b142b616a03d28e8d5e9190f536826faabb47b9
Junhsiun <xjuunn@gmail.com>
2025-10-16 14:45:34 +0800

    feat:封装基础list-content布局组件

---
+++ apps/frontend/app/components/layout/list-content/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="h-full flex">
+        <div v-show="isShowList || md" class="border-r border-base-content/10" :class="(isShowContent || md) ? 'w-[300px]' : 'flex-1'">
+            <slot name="list" />
+        </div>
+        <div v-show="isShowContent || md" class="flex-1">
+            <slot name="content" />
+        </div>
+    </div>
+</template>
+<script lang="ts" setup>
+import { breakpointsTailwind } from '@vueuse/core';
+const { md } = useBreakpoints(breakpointsTailwind);
+const isShowList = ref(true);
+const isShowContent = ref(true);
+const route = useRoute();
+watch(() => route.query.ui, () => {
+    switch (route.query.ui) {
+        case 'list':
+            isShowList.value = true;
+            isShowContent.value = false;
+            break;
+        case 'content':
+            isShowList.value = false;
+            isShowContent.value = true;
+            break;
+        default:
+            isShowList.value = true;
+            isShowContent.value = true;
+    }
+}, { immediate: true })
+
+
+</script>
--- apps/frontend/app/pages/index.vue
+++ apps/frontend/app/pages/index.vue
@@ -1,7 +1,12 @@
 <template>
-    <div>
-        index
-    </div>
+    <layout-list-content>
+        <template #list>
+            list
+        </template>
+        <template #content>
+            content
+        </template>
+    </layout-list-content>
 </template>
 <script lang="ts" setup>
 definePageMeta({ layout: "main-window" })
