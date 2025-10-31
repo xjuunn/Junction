4a7fc123ab914fc568a16fe0206a7b91a5f67795
Junhsiun <xjuunn@gmail.com>
2025-10-31 21:04:34 +0800

    style:使用更加简洁的输入框

--- apps/frontend/app/pages/auth/sign-in/index.vue
+++ apps/frontend/app/pages/auth/sign-in/index.vue
@@ -9,15 +9,11 @@
         <div class="divider"></div>
         <div class="flex flex-col gap-2">
             <label class="input bg-transparent focus-within:outline-0">
-                <span class="label">
-                    邮箱<icon name="mingcute:mail-fill"></icon>
-                </span>
+                <icon name="mingcute:mail-fill"></icon>
                 <input type="email" placeholder="请输入邮箱"></input>
             </label>
             <label class="input bg-transparent focus-within:outline-0">
-                <span class="label">
-                    密码<icon name="mingcute:lock-fill"></icon>
-                </span>
+                <icon name="mingcute:lock-fill"></icon>
                 <input type="password" placeholder="请输入密码">
             </label>
             <div class="inline-flex items-center gap-2 justify-between">
@@ -36,15 +32,15 @@
         </div>
         <div class="divider my-7">OR</div>
         <div class="grid grid-cols-3 text-base-content/80">
-            <nuxt-link to="sign-in/passkey" class="btn btn-sm btn-ghost">
+            <nuxt-link to="/auth/sign-in/passkey" class="btn btn-sm btn-ghost">
                 <icon name="mingcute:fingerprint-fill"></icon>
                 通行密钥
             </nuxt-link>
-            <nuxt-link to="sign-in/device-authorization" class="btn btn-sm btn-ghost">
+            <nuxt-link to="/auth/sign-in/device-authorization" class="btn btn-sm btn-ghost">
                 <icon name="mingcute:device-fill"></icon>
                 设备授权
             </nuxt-link>
-            <nuxt-link to="sign-in/magic-link" class="btn btn-sm btn-ghost">
+            <nuxt-link to="/auth/sign-in/magic-link" class="btn btn-sm btn-ghost">
                 <icon name="mingcute:link-fill"></icon>
                 魔术链接
             </nuxt-link>
---
+++ apps/frontend/app/pages/auth/sign-up/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div class="text-3xl w-full inline-flex items-center gap-2">
+            <span class="font-bold font-siyuan">注册</span>
+            <span class="font-modernia text-base-content/20 translate-y-1.5">
+                SIGNUP
+            </span>
+        </div>
+        <div class="divider"></div>
+        <div class="flex flex-col gap-2">
+            <label class="input bg-transparent focus-within:outline-0">
+                <icon name="mingcute:mail-fill"></icon>
+                <input type="email" placeholder="请输入邮箱"></input>
+            </label>
+            <label class="input bg-transparent focus-within:outline-0">
+                <icon name="mingcute:lock-fill"></icon>
+                <input type="password" placeholder="请输入密码">
+            </label>
+            <label class="input bg-transparent focus-within:outline-0">
+                <icon name="mingcute:lock-fill"></icon>
+                <input type="password" placeholder="请重复输入密码">
+            </label>
+            <div class="inline-flex items-center gap-2 justify-between">
+
+            </div>
+        </div>
+        <div class="mt-2">
+            <button class="btn btn-primary btn-block">下一步</button>
+        </div>
+    </div>
+</template>
