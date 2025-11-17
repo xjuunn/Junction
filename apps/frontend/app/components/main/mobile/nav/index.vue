525260af9d5082c0760aa83be52275d8785145a2
Junhsiun <xjuunn@gmail.com>
2025-11-17 20:29:34 +0800

    fix:修复手机端顶部被状态栏遮挡的问题

--- apps/frontend/app/components/main/mobile/nav/index.vue
+++ apps/frontend/app/components/main/mobile/nav/index.vue
@@ -1,7 +1,10 @@
 <template>
     <client-only>
-        <div v-motion-slide-top class="navbar min-h-[2.8rem] px-3 select-none" v-if="!md" data-tauri-drag-region>
+        <div v-motion-slide-top class="navbar min-h-[2.8rem] px-3 select-none" :class="{
+            'pt-6': hasMobileTaskBar
+        }" v-if="!md" data-tauri-drag-region>
             <div class="navbar-start">
+                {{ hasMobileTaskBar }}
                 <slot name="start"></slot>
             </div>
             <div class="navbar-end">
@@ -13,4 +16,8 @@
 
 <script setup lang="ts">
 const { md } = useTailwindBreakpoints();
+const { isAndroid } = useDevice();
+
+// 判断是否有顶部状态栏，如果有，需要留出一定空间
+const hasMobileTaskBar = computed(() => isTauri() && isAndroid)
 </script>
