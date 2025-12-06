b6f6e20bcd3a7f1dcde2a190f21265a8253e87f8
Junhsiun <xjuunn@gmail.com>
2025-12-06 19:38:21 +0800

    fix:删除重复定义的useSocket，安装gsap

--- apps/frontend/app/core/socket/useSocket.ts
+++
@@ -1,7 +0,0 @@
-import { SocketClient } from "./socket.client";
-import type { SocketNamespaces } from "./socket.types";
-
-export function useSocket<N extends keyof SocketNamespaces>(namespace: N) {
-    const client = SocketClient.getInstance(namespace);
-    return client;
-}
