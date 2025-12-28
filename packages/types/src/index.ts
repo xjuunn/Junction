3db2673155dc5dfee0810750c1c95743bae93b29
Junhsiun <xjuunn@gmail.com>
2025-12-28 21:08:20 +0800

    Revert "fix:修复prisma生成位置不正确的问题"
    This reverts commit e12c3d1a58413e9549c852a0492067dba51a80d3.
    
    revert:撤回错误的配置

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -2,9 +2,6 @@ export interface APP {
     name: string;
 }
 
-export type { Prisma } from '@prisma/client'
-export * from '@prisma/client'
-
 /** Prisma 类型 */
 export type * as PrismaTypes from 'prismaclient';
 export * as PrismaValues from 'prismaclient';
