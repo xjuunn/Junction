8aa6367003b813863535940dec74814686c82f36
Junhsiun <xjuunn@gmail.com>
2025-12-29 11:12:48 +0800

    test:部署测试环境变量

--- apps/frontend/app/pages/test.vue
+++ apps/frontend/app/pages/test.vue
@@ -1,9 +1,13 @@
 <template>
     <div>
-        test
+        <button class="btn btn-primary" @click="test">test</button>
     </div>
 </template>
 
 <script setup lang="ts">
+const runtimeConfig = useRuntimeConfig();
+function test() {
+    console.log(runtimeConfig.public);
 
+}
 </script>
